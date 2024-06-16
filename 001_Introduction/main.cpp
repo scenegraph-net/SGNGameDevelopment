c1fb130cac0642c8b4eba08d2fd6843dd341967f
alfred9797 <alfred9797@gmail.com>
2024-06-16 10:59:16 +0200

    Introduction: Drawing graphics

--- 001_Introduction/main.cpp
+++ 001_Introduction/main.cpp
@@ -2,21 +2,75 @@
 #include <windows.h>
 
 #include <string>
+#include <vector>
+
+
+void DrawToWindow(HWND windowHandle)
+{
+   RECT clientRect;
+   PAINTSTRUCT paintStruct;
+
+   HDC deviceContext = BeginPaint(windowHandle, &paintStruct);
+
+   // Draw white background
+   GetClientRect(windowHandle, &clientRect);
+   FillRect(deviceContext, &clientRect, static_cast<HBRUSH>(WHITE_BRUSH));
+
+   // Draw outlined shapes
+   Rectangle(deviceContext, 20, 20, 150, 150);
+   Ellipse(deviceContext, 180, 20, 400, 150);
+
+   // Draw a filled rectangle
+   HBRUSH redBrush = CreateSolidBrush(RGB(255, 0, 0));
+   RECT redRectangle{ 20, 170, 150, 300 };
+   FillRect(deviceContext, &redRectangle, redBrush);
+   DeleteObject(redBrush);
+
+   // Draw a filled ellipse
+   HBRUSH blueBrush = CreateHatchBrush(HS_DIAGCROSS, RGB(0, 0, 255));
+   HBRUSH oldBrush = static_cast<HBRUSH>(SelectObject(deviceContext, blueBrush));
+   Ellipse(deviceContext, 180, 170, 400, 300);
+   SelectObject(deviceContext, oldBrush);
+   DeleteObject(blueBrush);
+
+   // Get the title of the window
+   size_t titleLength = GetWindowTextLength(windowHandle);
+   std::vector<char> windowTitle(titleLength + 1);
+   GetWindowText(windowHandle, windowTitle.data(), static_cast<int>(titleLength + 1));
+
+   // Create a font and use it for drawing the window title
+   HFONT textFont = CreateFont(32, 20, 0, 0, FW_REGULAR, FALSE, FALSE, FALSE, ANSI_CHARSET, OUT_TT_PRECIS,
+      CLIP_DEFAULT_PRECIS, CLEARTYPE_QUALITY, DEFAULT_PITCH, "Arial");
+
+   HFONT oldFont = static_cast<HFONT>(SelectObject(deviceContext, textFont));
+
+   RECT textLocalRect;
+   DrawText(deviceContext, windowTitle.data(), static_cast<int>(windowTitle.size()), &textLocalRect, DT_CALCRECT);
+
+   int textWidth = textLocalRect.right - textLocalRect.left;
+   int textHeight = textLocalRect.bottom - textLocalRect.top;
+
+   RECT textDrawRect;
+   textDrawRect.left = 20;
+   textDrawRect.right = 20 + textWidth;
+   textDrawRect.top = 450;
+   textDrawRect.bottom = 450 + textHeight;
+
+   DrawText(deviceContext, windowTitle.data(), static_cast<int>(windowTitle.size()), &textDrawRect, DT_CENTER);
+
+   SelectObject(deviceContext, oldFont);
+   DeleteObject(textFont);
+
+   EndPaint(windowHandle, &paintStruct);
+}
 
 
 LRESULT CALLBACK WindowProc(HWND windowHandle, UINT message, WPARAM wParam, LPARAM lParam)
 {
-   RECT clientRect;
-   HDC deviceContext;
-   PAINTSTRUCT paintStruct;
-
    switch (message)
    {
       case WM_PAINT:
-         deviceContext = BeginPaint(windowHandle, &paintStruct);
-         GetClientRect(windowHandle, &clientRect);
-         FillRect(deviceContext, &clientRect, static_cast<HBRUSH>(WHITE_BRUSH));
-         EndPaint(windowHandle, &paintStruct);
+         DrawToWindow(windowHandle);
          return 0;
       case WM_DESTROY:
          PostQuitMessage(0);
