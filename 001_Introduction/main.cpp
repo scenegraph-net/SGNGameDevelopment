79a27f6191220aa2781167106b9f7d79f8cab2f8
alfred <alfred9797@gmail.com>
2024-10-20 18:32:32 +0200

    Introduction: Animation

--- 001_Introduction/main.cpp
+++ 001_Introduction/main.cpp
@@ -4,6 +4,8 @@
 #include <string>
 #include <vector>
 
+static float g_time = .0f;
+
 
 void DrawToWindow(HWND windowHandle)
 {
@@ -12,24 +14,30 @@ void DrawToWindow(HWND windowHandle)
 
    HDC deviceContext = BeginPaint(windowHandle, &paintStruct);
 
+   // Calculate displacements
+   constexpr float AMPLITUDE = 100.f;
+   constexpr float TIMESCALE = 3.f;
+   const int displacement1 = static_cast<int>(AMPLITUDE + sin(g_time * TIMESCALE) * AMPLITUDE);
+   const int displacement2 = static_cast<int>(AMPLITUDE + cos(g_time * TIMESCALE) * AMPLITUDE);
+
    // Draw white background
    GetClientRect(windowHandle, &clientRect);
    FillRect(deviceContext, &clientRect, static_cast<HBRUSH>(WHITE_BRUSH));
 
    // Draw outlined shapes
-   Rectangle(deviceContext, 20, 20, 150, 150);
-   Ellipse(deviceContext, 180, 20, 400, 150);
+   Rectangle(deviceContext, 20 + displacement1, 20, 150 + displacement1, 150);
+   Ellipse(deviceContext, 180 + displacement1, 20, 400 + displacement1, 150);
 
    // Draw a filled rectangle
    HBRUSH redBrush = CreateSolidBrush(RGB(255, 0, 0));
-   RECT redRectangle{ 20, 170, 150, 300 };
+   RECT redRectangle{ 20 + displacement2, 170, 150 + displacement2, 300 };
    FillRect(deviceContext, &redRectangle, redBrush);
    DeleteObject(redBrush);
 
    // Draw a filled ellipse
    HBRUSH blueBrush = CreateHatchBrush(HS_DIAGCROSS, RGB(0, 0, 255));
    HBRUSH oldBrush = static_cast<HBRUSH>(SelectObject(deviceContext, blueBrush));
-   Ellipse(deviceContext, 180, 170, 400, 300);
+   Ellipse(deviceContext, 180 + displacement2, 170, 400 + displacement2, 300);
    SelectObject(deviceContext, oldBrush);
    DeleteObject(blueBrush);
 
@@ -51,10 +59,10 @@ void DrawToWindow(HWND windowHandle)
    int textHeight = textLocalRect.bottom - textLocalRect.top;
 
    RECT textDrawRect;
-   textDrawRect.left = 20;
-   textDrawRect.right = 20 + textWidth;
-   textDrawRect.top = 450;
-   textDrawRect.bottom = 450 + textHeight;
+   textDrawRect.left = 20 + displacement1;
+   textDrawRect.right = 20 + textWidth + displacement1;
+   textDrawRect.top = 450 + displacement2;
+   textDrawRect.bottom = 450 + textHeight + displacement2;
 
    DrawText(deviceContext, windowTitle.data(), static_cast<int>(windowTitle.size()), &textDrawRect, DT_CENTER);
 
@@ -71,6 +79,10 @@ LRESULT CALLBACK WindowProc(HWND windowHandle, UINT message, WPARAM wParam, LPAR
    {
       case WM_PAINT:
          DrawToWindow(windowHandle);
+         return 0;
+      case WM_TIMER:
+         g_time += .02f;
+         InvalidateRect(windowHandle, nullptr, TRUE);
          return 0;
       case WM_DESTROY:
          PostQuitMessage(0);
@@ -101,8 +113,21 @@ int APIENTRY WinMain(HINSTANCE instance, HINSTANCE prevInstance, LPSTR commandLi
       return 0;
    }
 
-   HWND mainWindow = CreateWindow(windowClassName.c_str(), mainWindowTitle.c_str(), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT,
-      CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, nullptr, nullptr, instance, nullptr);
+   SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
+
+   constexpr int WINDOW_WIDTH = 1280;
+   constexpr int WINDOW_HEIGHT = 720;
+   constexpr DWORD WINDOW_STYLE = WS_OVERLAPPEDWINDOW;
+   constexpr DWORD WINDOW_EX_STYLE = 0;
+
+   RECT windowArea = { 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT };
+   AdjustWindowRectEx(&windowArea, WINDOW_STYLE, FALSE, WINDOW_EX_STYLE);
+
+   const int adjustedWidth = windowArea.right - windowArea.left;
+   const int adjustedHeight = windowArea.bottom - windowArea.top;
+
+   HWND mainWindow = CreateWindowEx(WINDOW_EX_STYLE, windowClassName.c_str(), mainWindowTitle.c_str(), WINDOW_STYLE,
+      CW_USEDEFAULT, CW_USEDEFAULT, adjustedWidth, adjustedHeight, nullptr, nullptr, instance, nullptr);
 
    if (nullptr == mainWindow)
    {
@@ -112,6 +137,8 @@ int APIENTRY WinMain(HINSTANCE instance, HINSTANCE prevInstance, LPSTR commandLi
 
    ShowWindow(mainWindow, showMode);
    UpdateWindow(mainWindow);
+
+   SetTimer(mainWindow, 0, 20, nullptr);
 
    MSG message;
 
